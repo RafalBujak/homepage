f84911273fb247dee103ecc254cfb42f1ad4634b
bujak.rafal <bujak.rafal@yahoo.com>
2021-07-21 10:33:44 +0200

    new project html

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Rafal';
+const age = 39;
+
+alert('Witam na mojej stronie, Mam na imię Rafał imam nadzieje, że znajdziesz tu wieleciekawych tresci');
+console.log(firstName);  
+console.log(age);
+console.log(`siema, nazywam sie ${firstName} i mam ${age} lat`);
+
